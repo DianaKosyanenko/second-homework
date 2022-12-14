6d58f1473d38d6a69646aee2ba449ba5d1956b42
Diana <kosyanenkodiana@yandex.ru>
2022-12-14 22:57:40 +0300

    new rep

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/*int Test(){
+    int num = new Random().Next(100,1000);
+    Console.WriteLine(num);
+    int hund = num /10;
+    return(num %10);
+}
+
+int res = Test();
+Console.WriteLine(res);*/
+
+/*int Test(){
+    int num = new Random().Next(100,1000);
+    Console.WriteLine(num);
+    int hund = num /100;
+    int dec = num /10;
+    return(num %100) / 10;
+}
+
+int res = Test();
+Console.WriteLine(res);*/
+
+
+
+/*void Test(int num)
+{
+    if (num > 7)
+    {
+       Console.WriteLine("Такого дня недели нет"); 
+    }
+    else if (num == 6 || num == 7)
+    {
+       Console.WriteLine("Сегодня выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Будни");
+    }
+    
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Test(num);*/
